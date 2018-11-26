42271909956fa18204fdd77760358de9948d4d5f
Carlos Obregon <cobregon@besserenergy.com>
2018-11-26 17:52:29 -0600

    Add sidebar menu

--- TarjetaFeria/js/menujs.js
+++ TarjetaFeria/js/menujs.js
@@ -1,2 +1,7 @@
+const headerSidebar = document.querySelector('.sidebar__menu');
+const toggleButton = document.querySelector('.toggle__button');
 
+toggleButton.addEventListener('click', function(){
+    headerSidebar.classList.toggle('active__sidebar');
+});
 
