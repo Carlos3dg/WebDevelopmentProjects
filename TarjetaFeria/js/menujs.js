d051b9ac4eac6bfe091355c46c2688e3c9889af2
Carlos Obregon <cobregon@besserenergy.com>
2018-11-29 18:02:56 -0600

    Finish funtionality and design in header navigation

--- TarjetaFeria/js/menujs.js
+++ TarjetaFeria/js/menujs.js
@@ -1,7 +1,23 @@
-const headerSidebar = document.querySelector('.sidebar__menu');
+//VARIABLES
+const headerSidebar = document.querySelector('.sidebar__menu'); //Sidebar menu
 const toggleButton = document.querySelector('.toggle__button');
+const navigation = document.querySelector('.navigation'); //Nav element
 
+//Click event in toggle button to open the sidebar:
 toggleButton.addEventListener('click', function(){
-    headerSidebar.classList.toggle('active__sidebar');
+    //Creation of the shadow next to the sidebar
+    const shadow = document.createElement('div');
+    shadow.classList.add('active__shadow');
+    //Add class that open the sidebar 
+    headerSidebar.classList.add('active__sidebar');
+    //Insert the shadow element after the sidebar transition is completed
+    setTimeout(function() {
+        navigation.insertBefore(shadow, document.querySelector('.header__container'));
+    }, 500);
+    //Click event in the shadow to remove it and close the sidebar
+    shadow.addEventListener('click', function() {
+        shadow.remove();
+        headerSidebar.classList.remove('active__sidebar');
+    })
 });
 
