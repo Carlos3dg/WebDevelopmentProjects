83c1f4026e71f2f5f37a9fafe427cd899a3ba1c1
Carlos Obregon <cobregon@besserenergy.com>
2018-12-10 15:07:24 -0600

    Add inteeraction to the navigation links

--- TarjetaFeria/js/menujs.js
+++ TarjetaFeria/js/menujs.js
@@ -1,8 +1,19 @@
 //VARIABLES
+/* Header elements */
 const headerSidebar = document.querySelector('.sidebar__menu'); //Sidebar menu
 const toggleButton = document.querySelector('.toggle__button');
 const navigation = document.querySelector('.navigation'); //Nav element
+const navLinks = document.querySelector('.container__nav-links');
+const sesionLink = document.getElementById('sesion');
+const indexLink = document.getElementById('index');
+const helpLink = document.getElementById('help');
 
+// Content elements
+const sesionContainer =  document.querySelector('.sesion__container');
+const indexContainer = document.querySelector('.index__container');
+const helpContainer = document.querySelector('.help__container');
+
+/* EVENT LISTENERS */
 //Click event in toggle button to open the sidebar:
 toggleButton.addEventListener('click', function(){
     //Creation of the shadow next to the sidebar
@@ -21,3 +32,27 @@ toggleButton.addEventListener('click', function(){
     })
 });
 
+//Click event in every link from the navigation to open the correct landing
+navLinks.addEventListener('click', function(e) {
+    e.preventDefault();
+    //Condition to evaluate the link that is being clicked (e.target)
+    if(e.target === sesionLink) {
+        //Remove the landing showed by the past nav link
+        document.querySelector('.show__element').classList.remove('show__element');
+        //Remove the border and font-weight from the nav link that was clicked before
+        document.querySelector('.nav__link-clicked').classList.remove('nav__link-clicked');
+        //Add the correct classes to show the landing correspondent and the font-weight and border in the link
+        sesionLink.classList.add('nav__link-clicked');
+        sesionContainer.classList.add('show__element');
+    } else if (e.target === indexLink) {
+        document.querySelector('.show__element').classList.remove('show__element');
+        document.querySelector('.nav__link-clicked').classList.remove('nav__link-clicked');
+        indexLink.classList.add('nav__link-clicked');
+        indexContainer.classList.add('show__element')
+    } else {
+        document.querySelector('.show__element').classList.remove('show__element');
+        document.querySelector('.nav__link-clicked').classList.remove('nav__link-clicked');
+        helpLink.classList.add('nav__link-clicked');
+        helpContainer.classList.add('show__element');
+    }
+})
