d580aaf73ce51c414338454b5f96959fb500a223
Carlos Obregon <cobregon@besserenergy.com>
2019-01-09 17:09:57 -0600

    TarjetaFeria: Modify navLinks code to OOP

--- TarjetaFeria/js/menujs.js
+++ TarjetaFeria/js/menujs.js
@@ -3,15 +3,8 @@
 const headerSidebar = document.querySelector('.sidebar__menu'); //Sidebar menu
 const toggleButton = document.querySelector('.toggle__button');
 const navLinks = document.querySelector('.container__nav-links');
-const sesionLink = document.getElementById('sesion');
-const indexLink = document.getElementById('index');
-const helpLink = document.getElementById('help');
 const searchButton = document.querySelector('.search__button');
 const searchClose = document.querySelector('.search-close__button');
-// Content elements
-const sesionContainer =  document.querySelector('.sesion__container');
-const indexContainer = document.querySelector('.index__container');
-const helpContainer = document.querySelector('.help__container');
 
 //CLASSES
 class UI {
@@ -71,27 +64,38 @@ searchClose.addEventListener('click', function() {
 //Click event in every link from the navigation to open the correct landing
 navLinks.addEventListener('click', function(e) {
     e.preventDefault();
+    const sesionLink = document.getElementById('sesion');
+    const indexLink = document.getElementById('index');
+    const helpLink = document.getElementById('help');
+
+    const sesionContainer =  document.querySelector('.sesion__container');
+    const indexContainer = document.querySelector('.index__container');
+    const helpContainer = document.querySelector('.help__container');
+    const ui = new UI();
     //Condition to evaluate the link that is being clicked (e.target)
     if(e.target === sesionLink) {
         //Remove the landing showed by the past nav link
-        document.querySelector('.show__element').classList.remove('show__element');
+        ui.hideElement(document.querySelector('.show__element'), 'show__element');
         //Remove the border and font-weight from the nav link that was clicked before
-        document.querySelector('.nav__link-clicked').classList.remove('nav__link-clicked');
+        ui.hideElement(document.querySelector('.nav__link-clicked'), 'nav__link-clicked');
         //Add the correct classes to show the landing correspondent and the font-weight and border in the link
         sesionLink.classList.add('nav__link-clicked');
         sesionContainer.classList.add('show__element');
+
     } else if (e.target === indexLink) {
-        document.querySelector('.show__element').classList.remove('show__element');
-        document.querySelector('.nav__link-clicked').classList.remove('nav__link-clicked');
+        ui.hideElement(document.querySelector('.show__element'), 'show__element');
+        ui.hideElement(document.querySelector('.nav__link-clicked'), 'nav__link-clicked');
+
         indexLink.classList.add('nav__link-clicked');
-        indexContainer.classList.add('show__element')
+        indexContainer.classList.add('show__element');
+        
     } else if (e.target === helpLink) {
-        document.querySelector('.show__element').classList.remove('show__element');
-        document.querySelector('.nav__link-clicked').classList.remove('nav__link-clicked');
+        ui.hideElement(document.querySelector('.show__element'), 'show__element');
+        ui.hideElement(document.querySelector('.nav__link-clicked'), 'nav__link-clicked');
+
         helpLink.classList.add('nav__link-clicked');
         helpContainer.classList.add('show__element');
 
-        const ui = new UI();
         ui.showSearchBar('help', '¿Cómo podemos ayudarte?');
     }
 });
